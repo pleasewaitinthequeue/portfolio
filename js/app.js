630cb84c8999435f0ec0f7429353b074c13ae06f
John Lairson <none.shall.pass.2009@gmail.com>
2026-03-01 10:28:24 -0500

    Content Updates, Styling Updates, Email Updates.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 //jQuery 3.3.1 min
 
-$("a").click(function(){
-   var idVal = $(this).attr("href");
-   $("html, body").animate({
-      'scrollTop':$(idVal).offset().top}, 250);
+$("a").click(function () {
+    var idVal = $(this).attr("href");
+    $("html, body").animate({
+        'scrollTop': $(idVal).offset().top
+    }, 250);
 });
 
-$(".contact").click(function(){
+$(".contact").click(function () {
     var contactDIV = $("<div></div>");
     var h1 = $("<h1>John Lairson</h1>");
-    var mailto = $("<a>john.lairson@johnlairson.com</a>");
-    mailto.prop("href", "mailto:john.lairson@johnlairson.com");
+    var mailto = $("<a>none.shall.pass.2009@gmail.com</a>");
+    mailto.prop("href", "none.shall.pass.2009@gmail.com");
     var message = $("<p>Click this Box to Close</p>");
     contactDIV.append(h1, mailto, message);
     contactDIV.addClass("contactme");
@@ -20,23 +21,24 @@ $(".contact").click(function(){
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor:"#000000",
-        color:"#ffffff",
-        height:"70%",
-        width:"70%",
-        margin:"0px",
-        zIndex:"100",
-        top:"25%",
-        left:"25%",
-        position:"fixed",
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        height: "70%",
+        width: "70%",
+        margin: "0px",
+        zIndex: "100",
+        top: "25%",
+        left: "25%",
+        position: "fixed",
     });
 
     $("html, body").animate({
-        'scrollTop':$("#summary").offset().top}, 250);
-    contactDIV.click(function(){
+        'scrollTop': $("#summary").offset().top
+    }, 250);
+    contactDIV.click(function () {
         this.remove();
     });
     $(".contactme").remove();
     $(".contact-container").append(contactDIV);
 
-    });
+});
